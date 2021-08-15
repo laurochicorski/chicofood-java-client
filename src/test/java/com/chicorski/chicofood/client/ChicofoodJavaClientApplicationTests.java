937b4739e8115788233cc294f8833702ad2fbfe8
Lauro Chicorski <laurocoelhoc@gmail.com>
2021-08-15 16:36:47 -0300

    Commit initial

---
+++ src/test/java/com/chicorski/chicofood/client/ChicofoodJavaClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chicorski.chicofood.client;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChicofoodJavaClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
